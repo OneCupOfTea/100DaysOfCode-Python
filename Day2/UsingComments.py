913af0a74ad99c08f69fe3d21fe990876f647597
najeebxx <najeebmoss@gmail.com>
2019-08-22 00:19:00 +0300

    Day2

---
+++ Day2/UsingComments.py
@@ -0,0 +1,9 @@
+"""
+We use triple quotation marks
+for multiline comment
+"""
+# We use this character( # ) in the beginning to write a comment in a single line.
+# Also it can be used to prevent Python from executing code.
+
+# print(5*4)
+print ("the result of multipling 5 with 4 is ",5*4)
