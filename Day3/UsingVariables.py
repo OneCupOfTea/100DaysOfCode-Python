7d31ecfc17a3b6b05cb364893cf984b47904ef6d
najeebxx <najeebmoss@gmail.com>
2019-08-22 14:00:58 +0300

    Day3

---
+++ Day3/UsingVariables.py
@@ -0,0 +1,26 @@
+# We use variables to store data (int, string, ...,etc) in them
+Fname= "Najeeb"
+Anystring= "started to learn python with SaudiDevOrg."
+
+# We use this character(+) to concatenate two or more strings
+print(Fname +" "+ Anystring)
+
+# Also we use this character(+) to add two or more numbers
+num1=100
+num2=500
+print("The result of adding 100 with 500 is ",num1+num2)
+
+# We can assign values to multiple variables in one line
+str1, str2= "Success", "begins with a step forward!"
+print(str1 +" "+ str2)
+
+# We can assign the same value to multiple variables in one line
+_var1 = _var2= "Progrmming with python is piece of cake!"
+print(_var1)
+print(_var2)
+
+"""
+We can't combine a number with a string
+number, name= 20, "Ali"
+print(number + name)
+"""
