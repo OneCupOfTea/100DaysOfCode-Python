95e455a4d79325a52e84c9c027c5ca4ebc6e1a58
najeebxx <najeebmoss@gmail.com>
2019-09-02 15:18:48 +0300

    Day8

---
+++ Day8/StringsPart2.py
@@ -0,0 +1,24 @@
+# use strip() function to remove any whitespace from the beginning or the end.
+str1= "    Hello world! "
+print(str1)
+print(str1.strip())
+
+# use len() function to git the length of a string.
+name = "Najeeb"
+print("The length of name variable value is",len(name))
+
+# use lower() function to git a string in lower case.
+str1= "PYTHON"
+print(str1.lower())
+
+# use upper() function to git a string in upper case.
+str1= "python"
+print(str1.upper())
+
+# use replace() function to replace a string with another string.
+str1= "Python is difficult"
+print(str1.replace("difficult","easy"))
+
+# use split() function to split a string into substrings by using a separator character.
+str1= "python is easy"
+print(str1.split(" "))
