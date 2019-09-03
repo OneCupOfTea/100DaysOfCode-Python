06dc10599fe17f33f90d9ea717e137407e75eee5
najeebxx <najeebmoss@gmail.com>
2019-09-03 10:06:21 +0300

    Day10

---
+++ Day10/OperatorsPart1.py
@@ -0,0 +1,17 @@
+x,y,z= 5,4,10
+
+print("Some examples of Arithmetic Operators:")
+print("5 * 5 =", x * x)     # Multiplication
+print("5 // 4 =", x // y)   # Floor Division
+print("5 ** 10 =", x ** z)  # Exponentiation
+
+print("Some examples of  Assignment Operators:")
+print("(4 &= 3) =", y & 3)   
+print("(10 ^= 13) =", z ^ 13)
+print("(5 >>= 20) =", x >> 20)
+print("(5 <<= 20) =", x << 20)
+
+print("Some examples of Comparison Operators:")
+print("10 > 5 ?", z > x)
+print("4 <= 10 ?", y <= z)
+print("4 != 4 ?", y != y)
